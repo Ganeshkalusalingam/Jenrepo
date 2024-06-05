393d6ba6774c608c53815fd0cbfdb8f7cae3b6e3
Ganeshkalusalingam <168487232+Ganeshkalusalingam@users.noreply.github.com>
2024-06-05 20:51:14 +0000

    Update Jenkin.java

--- Jenkin.java
+++ Jenkin.java
@@ -1,5 +1,5 @@
 public class Jenkin{
-public static void main(String[] args){
-System.out.println("Welcome to Ganesh's Magic show");
-}
+  public static void main(String[] args){
+    System.out.println("Welcome to Ganesh's Magic show");
+  }
 }
